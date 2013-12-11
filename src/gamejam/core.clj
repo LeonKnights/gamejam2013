d34349c3f6c8716f535932dbf913497587e869eb
Pance <pbarnett888@gmail.com>
2013-12-11 12:57:40 -0600

    Add implementation of Quil demo

--- src/gamejam/core.clj
+++ src/gamejam/core.clj
@@ -1,7 +1,30 @@
 (ns gamejam.core
-  (:gen-class))
+  (:gen-class)
+  (:use quil.core))
+
+(defn setup []
+  (smooth)
+  (frame-rate 1)
+  (background 200))
+
+(defn draw []
+  (stroke (random 255))
+  (stroke-weight (random 10))
+  (fill (random 255))
+
+  (let [diam (random 100)
+        x    (random (width))
+        y    (random (height))]
+    (ellipse x y diam diam)))
+
+(defn doSketch []
+  (defsketch example
+    :title "Window title"
+    :setup setup
+    :draw draw
+    :size [323 200]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (doSketch))
